2f59fd4197b926e870fbc44be0692ae583db5345
sasadusic <www.sasadusic0110989@gmail.com>
2024-02-12 22:21:05 +0100

    Deleting first calculator

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,5 @@
 import Quiz from './Quiz'
 import Todo from './Todo'
-import TicTac from './TicTac'
 import Game from './Game'
 import Calculator from './Calculator'
 import Slider from './Slider'
@@ -13,7 +12,6 @@ const Main = () => {
         <Slider url={"https://picsum.photos/v2/list"} page={"1"} limit={"4"} />
         <Calculator />
         <Game />
-        <TicTac />
         <Quiz />
         <Todo />
       </main>
